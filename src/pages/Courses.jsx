691848231ab23e279fe594f92fc962e76ce0fc82
Evmorfia Asimakopoulou <118734759+ssoc2easimakopoulou@users.noreply.github.com>
2023-03-03 03:17:45 +0200

    Courses Update #2
    Added a popup card modal with the description of each course.

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -6,13 +6,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck'
+import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 
 library.add(faCheckCircle, faTimesCircle);
 
+
 function Courses() {
   const [data, setData] = useState([]);
-
+  const [showModal, setShowModal] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+  
   useEffect(() => {
     async function fetchData() {
       try {
@@ -26,6 +30,15 @@ function Courses() {
     }
     fetchData();
   }, []);
+  
+  const handleCloseModal = () => {
+    setShowModal(false);
+  }
+
+  const handleShowModal = (course) => {
+    setSelectedData(course);
+    setShowModal(true);
+  }
 
   return (
     <CardDeck style={{ maxWidth: '900px', margin: '0 auto', display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
@@ -47,13 +60,39 @@ function Courses() {
              )}
              </div>
              <div style={{ marginLeft: '20px' }}>
-             <Button variant="dark" style={{ backgroundColor: 'black' }}>View</Button>
+             <Button variant="dark" style={{ backgroundColor: 'black' }} onClick={() => handleShowModal(item)}>View</Button>
              </div>
             </Card.Text>
-            
           </Card.Body>
         </Card>
       ))}
+      {selectedData && (
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Card style={{ backgroundColor: 'white' }}>
+            <Card.Img variant="top" src={selectedData.imagePath} alt={selectedData.title} />
+            <Card.Body>
+              <Card.Title><h4>{selectedData.title}</h4></Card.Title>
+              <Card.Text><strong>Dates:</strong> <i>{selectedData.dates.start_date}</i> - <i>{selectedData.dates.end_date}</i></Card.Text>
+              <Card.Text><strong>Duration: </strong>{selectedData.duration}</Card.Text>
+              <Card.Text>{selectedData.description}</Card.Text>
+              <Card.Text><strong>Price: </strong> <i>(Regular)</i> {selectedData.price.normal}  € <strong>|</strong> <i>(Early Bird Discount)</i> {selectedData.price.early_bird}  €</Card.Text>
+              <Card.Text style={{ display: 'flex', alignItems: 'center' }}>
+               <strong>Online:</strong>
+               <div style={{ marginLeft: '10px' }}>
+               {selectedData.online ? (
+               <FontAwesomeIcon icon="check-circle" size="lg" />
+               ) : (
+               <FontAwesomeIcon icon="times-circle" size="lg" />
+               )}
+               </div>
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
+            </Card.Footer>
+          </Card>
+        </Modal>
+      )}
     </CardDeck>
   );
 }
